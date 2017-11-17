9bb91b2532e965a72c51c61cc5b8021fa0fd90e5
cyberblast <git@cyberblast.org>
2017-11-17 22:46:25 +0100

    fixing deployment

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ echo "[INFO] Download nuget"
 wget -nc https://dist.nuget.org/win-x86-commandline/latest/nuget.exe;
 
 echo "[INFO] Create nuget package"
-mono nuget.exe pack cyberblast.SharePoint.Client.nuspec -properties version=1.0.$TRAVIS_BUILD_NUMBER && \
+mono nuget.exe pack cyberblast.SharePoint.Client.nuspec -properties version=$TRAVIS_TAG && \
 echo "[INFO] Deploy nuget package"
-mono nuget.exe push cyberblast.SharePoint.Client.1.0.$TRAVIS_BUILD_NUMBER.nupkg -ApiKey $NUGET_API_KEY -Verbosity detailed -Source https://www.nuget.org/api/v2/package 
+mono nuget.exe push cyberblast.SharePoint.Client.$TRAVIS_TAG.nupkg -ApiKey $NUGET_API_KEY -Verbosity detailed -Source https://www.nuget.org/api/v2/package 

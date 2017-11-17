f78a0cbdcc1c0128f07462e5d86d19a0c8c53a77
cyberblast <git@cyberblast.org>
2017-11-17 21:43:17 +0100

    download nuget

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+echo "[INFO] Download nuget"
+wget -nc https://dist.nuget.org/win-x86-commandline/latest/nuget.exe;
+mozroots --import --sync
+
+echo "[INFO] Create and deploy new nuget package"
 mono nuget.exe pack cyberblast.SharePoint.Client.nuspec && \
 mono nuget.exe push cyberblast.SharePoint.Client.nupkg -ApiKey $NUGET_API_KEY -Verbosity detailed -Source https://www.nuget.org/api/v2/package

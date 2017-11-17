f434d658cc9a43eca2e36b86c56efca4609151c5
cyberblast <git@cyberblast.org>
2017-11-17 21:58:50 +0100

    fixing deployment

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ mozroots --import --sync
 
 echo "[INFO] Create and deploy new nuget package"
 mono nuget.exe pack cyberblast.SharePoint.Client.nuspec && \
-mono nuget.exe push cyberblast.SharePoint.Client.nupkg -ApiKey $NUGET_API_KEY -Verbosity detailed -Source https://www.nuget.org/api/v2/package
+mono nuget.exe push cyberblast.SharePoint.Client.nupkg -ApiKey $NUGET_API_KEY -Verbosity detailed -Source https://www.nuget.org/api/v2/package -properties version=1.0.$TRAVIS_BUILD_NUMBER
